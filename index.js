bf912f237fa3b25b88f2434019e4172f7b7e7eca
akshatgarg12 <akshatarungarg78@gmail.com>
2023-02-10 14:53:04 +0530

    feat. added multiple paths

--- index.js
+++ index.js
@@ -8,19 +8,18 @@ const puppeteer = require("puppeteer");
 const pixelmatch = require('pixelmatch');
 const fs = require('fs');
 const PNG = require('pngjs').PNG
+const Links = require('./links.json');
+const { URL } = require("url");
+const { isArray } = require("util");
 
 class ScreenshotDiff{
-    constructor(url_1, url_2, debug = false){
+    constructor(url_1, url_2, browserConfig, debug = false, browser=null, close_browser_after_result = false){
         this.url_1 = url_1
         this.url_2 = url_2
-        this.browser = null
+        this.browser = browser
         this.debug = debug
-        this.browserConfig = {
-            defaultViewport: {
-              width: 1294,
-              height: 1280,
-            },
-        }
+        this.close_browser_after_result = close_browser_after_result
+        this.browserConfig = browserConfig
         const localhostScreenshots = __dirname + '/screenshots/localhost';
         const productionScreenshots = __dirname + '/screenshots/production';
         const diffScreenshots = __dirname + '/screenshots/diff';
@@ -64,13 +63,15 @@ class ScreenshotDiff{
        this.log('Browser opened')
        const page = await this.browser.newPage();
        this.log('New Page in browser opened')
+       console.log(url)
        await page.goto(url, {
-        waitUntil:"networkidle0"
+            waitUntil:"networkidle0",
+            timeout : 0
        })
        this.log('URL opened on page')
        // fileLocation ->  localhost_developers
        const filePath = __dirname + '/screenshots/' 
-       const domain = (parsedURL.hostname.indexOf('localhost') !== -1 ? 'localhost' : 'production') + '/'
+       const domain = (parsedURL.hostname.indexOf('localhost') !== -1 || parsedURL.hostname.indexOf('site-qwik') !== -1 ? 'localhost' : 'production') + '/'
        const fileName = this.getFileName(url)
        const fileLocation = filePath + domain + fileName
        await page.screenshot({path : fileLocation,type:"png"})
@@ -86,25 +87,49 @@ class ScreenshotDiff{
         console.log(image_1.height, image_2.height)
         const {height, width} = image_1
         const diff = new PNG({ width, height });
-        await this.puppeteer_browser_close()
-        this.log('Browser closed')
+        if(this.close_browser_after_result){
+            await this.puppeteer_browser_close()
+            this.log('Browser closed')
+        }
         const fileName = this.getFileName(this.url_1)
         pixelmatch(image_1.data, image_2.data, diff.data, width, height, { threshold: 0.7, includeAA: true });
         fs.writeFileSync(__dirname + `/screenshots/diff/${fileName}`, PNG.sync.write(diff));
     }
 }
 
-const helper = () => {
-    const localhost = 'http://localhost:5173/'
-    const production = 'https://www.builder.io/'
-    const pathnames = [
-        'c/docs/how-builder-works',
-        'c/docs/developers',
-        'c/docs/intro',
-    ]
+// const localhost = 'http://site-qwik.vercel.app'
+const localhost = 'http://localhost:5173'
+const production = 'https://www.builder.io'
+
+const getLinks = (links, result) => {
+    links.forEach((link) => {
+        if(link.subLinks){
+            getLinks(link.subLinks, result)
+        }else{
+            try{
+                const url = new URL(link.link)
+                result.push(url.pathname)
+            }catch(e){
+                result.push(link.link)
+            }
+        }
+    })
+    return result
+}
+
+const helper = async () => {
+    const pathnames = getLinks(Links, []).slice(0, 10)
+    console.log(pathnames)
+    const browserConfig = {
+        defaultViewport: {
+          width: 1294,
+          height: 1280,
+        },
+    }
+    const browser = await puppeteer.launch(browserConfig)
     pathnames.forEach((pathname) => {
-        new ScreenshotDiff(localhost+pathname, production+pathname).result()
+        new ScreenshotDiff(localhost+pathname, production+pathname, browserConfig, true, browser, false).result()
     })
 }
 
-helper()
+helper()  
