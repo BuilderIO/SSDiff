4ae0a7298be22992684ba688c6a0ed728b5a7544
akshatgarg12 <akshatarungarg78@gmail.com>
2023-03-04 15:59:20 +0530

    added readme and removed non-library code

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,16 @@ import puppeteer from "puppeteer";
 import pixelmatch from 'pixelmatch';
 import fs from 'fs';
 import { PNG } from 'pngjs';
-// import Links from '../links.json';
 import { URL } from "url";
 
-interface BrowserConfig {
+export interface BrowserConfig {
     defaultViewport: { width: number; height: number; };
 }
-interface ScreenshotConfig {
+export interface ScreenshotConfig {
     type: 'png' | 'jpeg' | 'webp';
     fullPage?: boolean;
 }
-interface ScreenshotDiffConfig {
+export interface ScreenshotDiffConfig {
     url_1: string;
     url_2: string;
     pathnames: string[];
@@ -137,42 +136,5 @@ class ScreenshotDiff{
     }
 }
 
-const localhost = 'http://site-qwik.vercel.app'
-// const localhost = 'http://localhost:5173'
-const production = 'https://www.builder.io'
 
-const getLinks = (links: any[], result: string[]) => {
-    links.forEach((link: { subLinks: any; link: string }) => {
-        if(link.subLinks){
-            getLinks(link.subLinks, result)
-        }else{
-            // only take the links that are in the docs
-            if(link.link.indexOf('/c/docs') !== -1){
-                try{
-                    const url = new URL(link.link)
-                    result.push(url.pathname)
-                }catch(e){
-                    result.push(link.link)
-                }
-            }   
-        }
-    })
-    return result
-}
-
-const helper = async () => {
-    // const pathnames = getLinks(Links, []).slice(140,165)
-    const pathnames = ['/c/docs/quickstart', '/c/docs/models-intro', '/c/docs/enterprise-hub', '/c/docs/getting-started']
-   
-    const config = {
-        url_1: localhost,
-        url_2: production,
-        pathnames,
-    }
-    const ssDiff = new ScreenshotDiff(config)
-    const result = await ssDiff.result()
-    console.log(result)
-}
-
-helper()  
-
+export default ScreenshotDiff
