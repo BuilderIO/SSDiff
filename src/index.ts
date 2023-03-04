2c84d273dbb979c6e0e272341d17e45b63c9bd6b
akshatgarg12 <akshatarungarg78@gmail.com>
2023-03-04 14:02:31 +0530

    feat. added typescript support

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,25 @@
 // ISSUES : 
 // page size needs to be same
 // Position of pixels
-const puppeteer = require("puppeteer");
-const pixelmatch = require('pixelmatch');
-const fs = require('fs');
-const PNG = require('pngjs').PNG
-const Links = require('./links.json');
-const { URL } = require("url");
+import puppeteer from "puppeteer";
+import pixelmatch from 'pixelmatch';
+import fs from 'fs';
+import { PNG } from 'pngjs';
+// import Links from '../links.json';
+import { URL } from "url";
 
 class ScreenshotDiff{
-    constructor(url_1, url_2, pathnames ,browserConfig, debug = false){
+    url_1: any;
+    url_2: any;
+    pathnames: any;
+    browser: any;
+    debug: boolean;
+    browserConfig: any;
+    screenshotsFolder: string;
+    todaysScreenshotFolder: string;
+    diffScreenshots: string;
+    
+    constructor(url_1: string, url_2: string, pathnames: string[] ,browserConfig: { defaultViewport: { width: number; height: number; }; }, debug = false){
         this.url_1 = url_1
         this.url_2 = url_2
         this.pathnames = pathnames
@@ -20,7 +30,7 @@ class ScreenshotDiff{
         this.debug = debug
         this.browserConfig = browserConfig
         // TODO: make the file naming dynamic based on hostnames
-        this.screenshotsFolder = __dirname + '/screenshots'
+        this.screenshotsFolder = process.cwd() + '/screenshots'
         const todaysDate = new Date().toISOString().split('T')[0]
         this.todaysScreenshotFolder = this.screenshotsFolder + '/' + todaysDate
         this.diffScreenshots = this.todaysScreenshotFolder + '/diff';
@@ -29,7 +39,7 @@ class ScreenshotDiff{
             this.log("Created diff folder for todays ss")
         }
     }
-    log(text){
+    log(text: string){
         if(this.debug){
             console.log(text)
         }
@@ -46,25 +56,25 @@ class ScreenshotDiff{
             this.browser = null
         }
     }
-    getFileName(url){
+    getFileName(url: string){
         const parsedURL = new URL(url)
         return parsedURL.pathname.split('/')[3]
     }
-    async screenshot(url){
+    async screenshot(url: any){
        const page = await this.browser.newPage();
        this.log('New Page in browser opened')
        await page.goto(url, {
             waitUntil:"networkidle0",
             timeout : 0
        })
-       this.log('URL opened on page ', url)
+       this.log(`URL opened on page ${url}`)
        // fileLocation ->  localhost_developers
        const screenshot = await page.screenshot({type:"png"})
        await page.close()
        this.log('Page closed')
        return screenshot
     }
-    async compare(compareObj){
+    async compare(compareObj: { url_1: any; url_2: any; fileName: any; }){
         const {url_1, url_2, fileName} = compareObj
         const screenshots = await Promise.all([this.screenshot(url_1), this.screenshot(url_2)])
         const image_1 = PNG.sync.read(screenshots[0]);
@@ -77,14 +87,14 @@ class ScreenshotDiff{
     async result(){
         await this.puppeteer_browser_open()
         this.log('Browser opened')
-        const urls = this.pathnames.map((pathname) => {
+        const urls = this.pathnames.map((pathname: any) => {
             return {
                 url_1 : this.url_1 + pathname,
                 url_2 : this.url_2 + pathname,
                 fileName : this.getFileName(this.url_1+pathname) 
             }
         })
-        const promises = urls.map((compareObj) => this.compare(compareObj))
+        const promises = urls.map((compareObj: any) => this.compare(compareObj))
         await Promise.all(promises)
         await this.puppeteer_browser_close()
         this.log('Browser closed')
@@ -95,8 +105,8 @@ const localhost = 'http://site-qwik.vercel.app'
 // const localhost = 'http://localhost:5173'
 const production = 'https://www.builder.io'
 
-const getLinks = (links, result) => {
-    links.forEach((link) => {
+const getLinks = (links: any[], result: string[]) => {
+    links.forEach((link: { subLinks: any; link: string }) => {
         if(link.subLinks){
             getLinks(link.subLinks, result)
         }else{
@@ -115,7 +125,8 @@ const getLinks = (links, result) => {
 }
 
 const helper = async () => {
-    const pathnames = getLinks(Links, []).slice(0,20)
+    // const pathnames = getLinks(Links, []).slice(140,165)
+    const pathnames = ['/c/docs/quickstart', '/c/docs/models-intro', '/c/docs/enterprise-hub']
     const browserConfig = {
         defaultViewport: {
           width: 1294,
@@ -127,3 +138,4 @@ const helper = async () => {
 }
 
 helper()  
+
