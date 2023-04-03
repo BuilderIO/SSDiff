ac73c923fb9e23eff9ca1680c13f59c45b77045a
akshatgarg12 <akshatarungarg78@gmail.com>
2023-04-03 14:53:23 +0530

    published sdk

--- src/__tests__/ssdiff.test.ts
+++ src/__tests__/ssdiff.test.ts
@@ -1,15 +1,15 @@
-import {describe, expect, test} from '@jest/globals';
+import { describe, expect, test } from '@jest/globals';
 import { PNG } from 'pngjs';
 import { SSDiff } from '../index';
 
 describe('SSDiff method testing', () => {
-  let config = {
-    url1 : 'http://site-qwik.vercel.app',
-    url2 : 'https://www.builder.io',
-    pathnames : ['/c/docs/developers', '/c/docs/quickstart']
-  }
-  let ssdiff = new SSDiff(config)
-  let screenshot: any = null
+  const config = {
+    url1: 'http://site-qwik.vercel.app',
+    url2: 'https://www.builder.io',
+    pathnames: ['/c/docs/developers', '/c/docs/quickstart'],
+  };
+  const ssdiff = new SSDiff(config);
+  let screenshot: any = null;
 
   test('default config values being set correctly', () => {
     expect(ssdiff.failInCaseOfDifferentSize).toBe(false);
@@ -25,50 +25,49 @@ describe('SSDiff method testing', () => {
       type: 'png',
     });
   });
-  
+
   test('test diffScreenshots is set correctly', () => {
     const todaysDate = new Date().toISOString().split('T')[0];
     const cwd = process.cwd();
     expect(ssdiff.diffScreenshots).toBe(`${cwd}/screenshots/${todaysDate}/diff`);
-  })
-  
+  });
+
   test('test browser open and close', async () => {
     // initially should be null
     expect(ssdiff.browser).toBe(null);
     await ssdiff.puppeteer_browser_open();
     expect(ssdiff.browser).not.toBe(null);
-    await ssdiff.puppeteer_browser_close()
+    await ssdiff.puppeteer_browser_close();
     expect(ssdiff.browser).toBe(null);
-  })
+  });
 
   test('test getFileName method', () => {
     expect(ssdiff.getFileName('http://site-qwik.vercel.app/c/docs/developers/')).toBe('developers');
     expect(ssdiff.getFileName('https://www.builder.io/c/docs/quickstart')).toBe('quickstart');
-  })
+  });
 
-  test('test screenshot method' , async () => {
+  test('test screenshot method', async () => {
     await ssdiff.puppeteer_browser_open();
     screenshot = await ssdiff.screenshot(config.url1 + config.pathnames[0]);
     expect(screenshot).toBeInstanceOf(Buffer);
     expect(screenshot).not.toBe(null);
-    await ssdiff.puppeteer_browser_close()
-  })
+    await ssdiff.puppeteer_browser_close();
+  });
 
   test('test resize method', async () => {
     await ssdiff.puppeteer_browser_open();
-    if(!screenshot){
-        fail('screenshot is null');
-        return
+    if (!screenshot) {
+      fail('screenshot is null');
+      return;
     }
-    const image = PNG.sync.read(screenshot)
-    const resized = await ssdiff.resizeImage(image, image.width+10, image.height+10);
-    expect(resized.height).toEqual(image.height+10);
-    expect(resized.width).toEqual(image.width+10);
-    await ssdiff.puppeteer_browser_close()
-  })
-
+    const image = PNG.sync.read(screenshot);
+    const resized = await ssdiff.resizeImage(image, image.width + 10, image.height + 10);
+    expect(resized.height).toEqual(image.height + 10);
+    expect(resized.width).toEqual(image.width + 10);
+    await ssdiff.puppeteer_browser_close();
+  });
 });
 
 function fail(arg0: string) {
-    throw new Error(arg0);
+  throw new Error(arg0);
 }
