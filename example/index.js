760bf38f0c5f1fcd4350667121080d774c098a42
akshatgarg12 <akshatarungarg78@gmail.com>
2023-04-26 15:42:51 +0530

    tested and added page config to the example

--- example/index.js
+++ example/index.js
@@ -1,47 +1,46 @@
-const Links = require('./links.json')
-const {SSDiff} = require('ssdiff')
-// const localhost = 'http://site-qwik.vercel.app'
-const localhost = 'http://localhost:5173'
-// const production = 'https://www.builder.io'
-const production = 'http://localhost:3000'
+const { SSDiff } = require('ssdiff');
+const localhost = 'http://site-qwik.vercel.app';
+const production = 'https://www.builder.io';
 
 // Function to filter links from an external source
 const getLinks = (links, result) => {
-    links.forEach((link) => {
-        if(link.subLinks){
-            getLinks(link.subLinks, result)
-        }else{
-            // only take the links that are in the docs
-            if(link.link.indexOf('/c/docs') !== -1){
-                try{
-                    const url = new URL(link.link)
-                    result.push(url.pathname)
-                }catch(e){
-                    result.push(link.link)
-                }
-            }   
+  links.forEach((link) => {
+    if (link.subLinks) {
+      getLinks(link.subLinks, result);
+    } else {
+      // only take the links that are in the docs
+      if (link.link.indexOf('/c/docs') !== -1) {
+        try {
+          const url = new URL(link.link);
+          result.push(url.pathname);
+        } catch (e) {
+          result.push(link.link);
         }
-    })
-    return result
-}
+      }
+    }
+  });
+  return result;
+};
 
 const helper = async () => {
-    // const pathnames = getLinks(Links, []).slice(160, 180)
-    const pathnames = ['/square']
-    // const pathnames = ['/c/docs/quickstart', '/c/docs/models-intro']
-    const ssDiff = new SSDiff({
-       url1: localhost,
-       url2: production,
-       pathnames,
-       screenshotConfig:{
-        fullPage: true,
-       },
-       debug : true,
-       outputFile: true
-    })
+  const pathnames = ['/c/docs/developers'];
+  const ssDiff = new SSDiff({
+    url1: localhost,
+    url2: production,
+    pageConfig: {
+      waitUntil: 'domcontentloaded',
+      timeout: 0,
+    },
+    pathnames,
+    screenshotConfig: {
+      fullPage: true,
+    },
+    debug: true,
+    outputFile: true,
+  });
 
-    const result = await ssDiff.result()
-    console.log(result)
-}
+  const result = await ssDiff.result();
+  console.log(result);
+};
 
-helper()  
+helper();
