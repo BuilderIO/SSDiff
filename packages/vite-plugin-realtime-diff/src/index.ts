c79cfbb7ed8e8762fbfa33a2427515963d5eb9ff
sidmohanty11 <sidmohanty11@gmail.com>
2023-12-08 20:45:27 +0530

    diff based on current localurl vs base url2

--- packages/vite-plugin-realtime-diff/src/index.ts
+++ packages/vite-plugin-realtime-diff/src/index.ts
@@ -3,7 +3,7 @@ import { generateHtmlTemplate } from "./template";
 import { getIframeWidth, isValidUrl } from "./utils";
 import type { ViteDevServer } from "vite";
 
-export function realtimeDiff(url1: string, url2: string) {
+export function realtimeDiff(url2: string) {
   return {
     name: "vite-plugin-realtime-diff",
     configureServer(server: ViteDevServer) {
@@ -13,9 +13,16 @@ export function realtimeDiff(url1: string, url2: string) {
           return;
         }
 
-        const { pathname, query } = parse(req.url, true);
+        const { query } = parse(req.url, true);
+        let url1 = `http://${req.headers.host}${req.url}`;
+        const pathname = req.url as string;
+        url2 = new URL(url2).origin + pathname;
 
-        if (pathname === "/_diff") {
+        if (query._diff && query._diff === "true") {
+          // Remove the _diff query param from the URL
+          url1 = url1.replace("?_diff=true", "");
+          url2 = url2.replace("?_diff=true", "");
+
           const widthKey = (query.width as string) || "100%";
           const iframeWidth = getIframeWidth(widthKey);
 
